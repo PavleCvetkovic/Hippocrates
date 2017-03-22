9f57c8e566d952760e0850220c28b2a31b90f53e
Djela <ika.djelic@gmail.com>
2017-03-22 21:19:35 +0100

    HippocratesDoctor zapocet

---
+++ HippocratesDoctor/HippocratesDoctor/FormDirektor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace HippocratesDoctor
+{
+    public partial class FormDirektor : MetroForm
+    {
+        public FormDirektor()
+        {
+            InitializeComponent();
+        }
+    }
+}
