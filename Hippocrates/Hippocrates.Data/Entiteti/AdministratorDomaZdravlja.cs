96dea905f4789a410a984298fc83e5e4ea4ff7f1
AndrijaCvetkovic <andrijacvetkovic@elfak.rs>
2017-04-06 20:48:35 +0200

    Funkcije za unos koriscenjem orm-a
    Trenutno samo funkcije za unos Domova Zdravlja i lekara.Takodje data grid view-u se pune koriscenjem orm-a.

---
+++ Hippocrates/Hippocrates.Data/Entiteti/AdministratorDomaZdravlja.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hippocrates.Data.Entiteti
+{
+    public class AdministratorDomaZdravlja
+    {
+        public virtual string JMBG { get; set; }
+        public virtual string Ime { get; set; }
+        public virtual string SrednjeSlovo { get; set; }
+        public virtual string Prezime { get; set; }
+        public virtual string MBRZU { get; set; }
+        public virtual string Password { get; set; }
+
+        public virtual DomZdravlja RadiUDomuZdravlja { get; set; }
+    }
+}
